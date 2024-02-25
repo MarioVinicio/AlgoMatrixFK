1fe2a5d07c24ee6434425f848ab68e5c8fe77126
Mario Davila <mario.vinicio@gmail.com>
2024-02-24 18:15:11 -0600

    Add Quick1 and Quick2 Sort Algorithm

--- tests/test_sort_algorithms.py
+++ tests/test_sort_algorithms.py
@@ -16,7 +16,16 @@ def test_radix_sort(sort_algorithms: SortAlgorithms, test_logger: Logger):
     Test case for Radix Sort Algorithm
     """
     test_logger.info("Sort Algorithm: RADIX")
-    assert sort_algorithms.radix_sort([170, 45, 75, 90, 802, 24, 2, 66]) == [2, 24, 45, 66, 75, 90, 170, 802]
+    assert sort_algorithms.radix_sort([170, 45, 75, 90, 802, 24, 2, 66]) == [
+        2,
+        24,
+        45,
+        66,
+        75,
+        90,
+        170,
+        802,
+    ]
 
 
 def test_bubble_sort(sort_algorithms: SortAlgorithms, test_logger: Logger):
@@ -24,4 +33,47 @@ def test_bubble_sort(sort_algorithms: SortAlgorithms, test_logger: Logger):
     Test case for Bubble Sort Algorithm
     """
     test_logger.info("Sort Algorithm: Bubble")
-    assert sort_algorithms.bubble_sort([170, 45, 75, 90, 802, 24, 2, 66]) == [2, 24, 45, 66, 75, 90, 170, 802]
+    assert sort_algorithms.bubble_sort([170, 45, 75, 90, 802, 24, 2, 66]) == [
+        2,
+        24,
+        45,
+        66,
+        75,
+        90,
+        170,
+        802,
+    ]
+
+
+def test_quick_sort1(sort_algorithms: SortAlgorithms, test_logger: Logger):
+    """
+    Test case for Quick Sort1 Algorithm
+    """
+    test_logger.info("Sort Algorithm: Quick1")
+    assert sort_algorithms.quick_sort1([170, 45, 75, 90, 802, 24, 2, 66]) == [
+        2,
+        24,
+        45,
+        66,
+        75,
+        90,
+        170,
+        802,
+    ]
+
+
+def test_quick_sort2(sort_algorithms: SortAlgorithms, test_logger: Logger):
+    """
+    Test case for Quick Sort2 Algorithm
+    """
+    test_logger.info("Sort Algorithm: Quick2")
+    assert sort_algorithms.quick_sort2([170, 45, 75, 90, 802, 24, 2, 66]) == [
+        2,
+        24,
+        45,
+        66,
+        75,
+        90,
+        170,
+        802,
+    ]
--- utils/sort_algorithms.py
+++ utils/sort_algorithms.py
@@ -1,3 +1,4 @@
+from random import randint
 from typing import List
 
 
@@ -56,12 +57,11 @@ class SortAlgorithms:
             exp *= 10  # Move to the next digit place
 
         return arr
-    
 
     def bubble_sort(self, arr: List[int]) -> List[int]:
         """
-        Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, 
-        compares adjacent elements, and swaps them if they are in the wrong order. 
+        Bubble Sort is a simple sorting algorithm that repeatedly steps through the list,
+        compares adjacent elements, and swaps them if they are in the wrong order.
         The pass through the list is repeated until the list is sorted.
 
         Sorts the input list in ascending order using the Bubble Sort algorithm.
@@ -77,3 +77,54 @@ class SortAlgorithms:
                 if arr[j] < arr[j - 1]:
                     arr[j], arr[j - 1] = arr[j - 1], arr[j]
         return arr
+
+    def quick_sort1(self, arr: List[int]) -> List[int]:
+        """
+        QuickSort is a highly efficient sorting algorithm that uses a divide-and-conquer strategy to sort elements in a list.
+        The basic idea is to choose a "pivot" element from the array and partition the other elements into two sub-arrays
+        according to whether they are less than or greater than the pivot. The sub-arrays are then recursively sorted.
+
+        Sorts the input list in ascending order using the QuickSort1 algorithm.
+
+        Parameters:
+        - arr (list): The input list to be sorted.
+
+        Returns:
+        - arr list: The sorted list.
+        """
+        if len(arr) <= 1:
+            return arr
+
+        smaller, equal, larger = [], [], []
+        pivot = arr[randint(0, len(arr) - 1)]
+
+        for x in arr:
+            if x < pivot:
+                smaller.append(x)
+            elif x == pivot:
+                equal.append(x)
+            else:
+                larger.append(x)
+
+        return self.quick_sort1(smaller) + equal + self.quick_sort1(larger)
+
+    def quick_sort2(self, arr: List[int]) -> List[int]:
+        """
+        Sorts the input list in ascending order using the QuickSort2 algorithm.
+
+        Parameters:
+        - arr (list): The input list to be sorted.
+
+        Returns:
+        - arr list: The sorted list.
+        """
+
+        if len(arr) <= 1:
+            return arr
+
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+
+        return self.quick_sort2(left) + middle + self.quick_sort2(right)
