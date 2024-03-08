736f7ca86ccff6b8300007144a58517faaba849b
Mario Davila <mario.vinicio@gmail.com>
2024-03-08 11:04:02 -0600

    [Feature] - Setting up fixtures and parameterizing tests
    -Added yaml files containing test parameters
    -Added numpy files to store test arrays
    -Setup fixtures for test parameters
    -Add hook for measuring sort time

--- conftest.py
+++ conftest.py
@@ -1,6 +1,10 @@
 import os
+import time
 import pytest
+import yaml
 import logging
+from utils.sort_algorithms import SortAlgorithms
+from logging import Logger
 
 """
 Using pytest hooks:
@@ -35,7 +39,6 @@ def test_logger(request):
 
     # Create a file to capture the test output
     log_file_path = f"{log_dir}/{test_name}.log"
-    print(f"\nLog file path: {log_file_path}")
     # Create a file handler for the test-specific log file
     file_handler = logging.FileHandler(log_file_path)
     file_handler.setLevel(logging.DEBUG)
@@ -53,3 +56,45 @@ def test_logger(request):
 
     # Optionally, perform teardown or cleanup actions after the test
     logger.removeHandler(file_handler)
+
+
+"""
+Run tests from YAML file
+"""
+
+YAML_FILE_PATH = "tests/sort_algorithms.yaml"
+
+
+def pytest_generate_tests(metafunc):
+    if "test_params" in metafunc.fixturenames:
+        with open(YAML_FILE_PATH, "r") as yaml_file:
+            data = yaml.safe_load(yaml_file)
+            # Provide the list of dictionaries containing parameters
+            metafunc.parametrize("test_params", data["tests"], ids=lambda x: x["name"])
+
+
+@pytest.fixture
+def test_params(request):
+    return request.param
+
+
+@pytest.fixture
+def sort_algorithms(measure_sort_time):
+    sort = SortAlgorithms()
+    # setup
+    yield sort
+    # teardown
+
+
+@pytest.fixture
+def measure_sort_time(request: pytest.FixtureRequest, test_logger: Logger, test_params):
+    start_time = time.time()
+
+    def finalizer():
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        test_logger.info(
+            f"Sorted array: {test_params['name']} - Sort time: {elapsed_time:.6f} seconds"
+        )
+
+    request.addfinalizer(finalizer)
--- tests/test_sort_algorithms.py
+++ tests/test_sort_algorithms.py
@@ -1,96 +1,53 @@
 from logging import Logger
-import time
-import pytest
 from utils.sort_algorithms import SortAlgorithms
+import numpy as np
 
 
-@pytest.fixture
-def sort_algorithms():
-    sort = SortAlgorithms()
-    # setup
-    yield sort
-    # teardown
+def numpy_data(file: str):
+    # load Numpy array from file
+    loaded_array = np.load(f"utils/test_arrays/{file}")
+    return loaded_array.tolist()
 
 
-@pytest.fixture
-def measure_sort_time(request, test_logger: Logger):
-    start_time = time.time()
-
-    def finalizer():
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        test_logger.info(f"Sort time: {elapsed_time:.6f} seconds")
-
-    request.addfinalizer(finalizer)
-
-
-@pytest.mark.usefixtures("measure_sort_time")
-def test_radix_sort(sort_algorithms: SortAlgorithms, test_logger: Logger):
+def test_radix_sort(sort_algorithms: SortAlgorithms, test_logger: Logger, test_params):
     """
     Test case for Radix Sort Algorithm
     """
     test_logger.info("Sort Algorithm: RADIX")
-    assert sort_algorithms.radix_sort([170, 45, 75, 90, 802, 24, 2, 66]) == [
-        2,
-        24,
-        45,
-        66,
-        75,
-        90,
-        170,
-        802,
-    ]
+    array_to_sort = numpy_data(test_params["array_to_sort"])
+    sorted_array = numpy_data(test_params["sorted_array"])
+
+    assert sort_algorithms.radix_sort(array_to_sort) == sorted_array
 
 
-@pytest.mark.usefixtures("measure_sort_time")
-def test_bubble_sort(sort_algorithms: SortAlgorithms, test_logger: Logger):
+def test_bubble_sort(sort_algorithms: SortAlgorithms, test_logger: Logger, test_params):
     """
     Test case for Bubble Sort Algorithm
     """
     test_logger.info("Sort Algorithm: Bubble")
-    assert sort_algorithms.bubble_sort([170, 45, 75, 90, 802, 24, 2, 66]) == [
-        2,
-        24,
-        45,
-        66,
-        75,
-        90,
-        170,
-        802,
-    ]
+    array_to_sort = numpy_data(test_params["array_to_sort"])
+    sorted_array = numpy_data(test_params["sorted_array"])
+
+    assert sort_algorithms.bubble_sort(array_to_sort) == sorted_array
 
 
-@pytest.mark.usefixtures("measure_sort_time")
-def test_quick_sort1(sort_algorithms: SortAlgorithms, test_logger: Logger):
+def test_quick_sort1(sort_algorithms: SortAlgorithms, test_logger: Logger, test_params):
     """
     Test case for Quick Sort1 Algorithm
     """
     test_logger.info("Sort Algorithm: Quick1")
-    assert sort_algorithms.quick_sort1([170, 45, 75, 90, 802, 24, 2, 66]) == [
-        2,
-        24,
-        45,
-        66,
-        75,
-        90,
-        170,
-        802,
-    ]
+    array_to_sort = numpy_data(test_params["array_to_sort"])
+    sorted_array = numpy_data(test_params["sorted_array"])
+
+    assert sort_algorithms.quick_sort1(array_to_sort) == sorted_array
 
 
-@pytest.mark.usefixtures("measure_sort_time")
-def test_quick_sort2(sort_algorithms: SortAlgorithms, test_logger: Logger):
+def test_quick_sort2(sort_algorithms: SortAlgorithms, test_logger: Logger, test_params):
     """
     Test case for Quick Sort2 Algorithm
     """
     test_logger.info("Sort Algorithm: Quick2")
-    assert sort_algorithms.quick_sort2([170, 45, 75, 90, 802, 24, 2, 66]) == [
-        2,
-        24,
-        45,
-        66,
-        75,
-        90,
-        170,
-        802,
-    ]
+    array_to_sort = numpy_data(test_params["array_to_sort"])
+    sorted_array = numpy_data(test_params["sorted_array"])
+
+    assert sort_algorithms.quick_sort2(array_to_sort) == sorted_array
